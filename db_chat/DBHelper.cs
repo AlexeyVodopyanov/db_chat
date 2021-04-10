1697cd235b529960405e2ac053532af66f54e933
Алексей <52876090+AlexeyVodopyanov@users.noreply.github.com>
2021-04-10 14:25:10 +0300

    Add files via upload

---
+++ db_chat/DBHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace db_chat
+{
+    public static class DBHelper
+    {
+        private static MySqlConnection connection;
+        private static MySqlCommand cmd = null;
+
+        public static void EstablishConnection()
+        {
+            try
+            {
+                MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+                builder.Server = "mysql60.hostland.ru";
+                builder.UserID = "host1323541_itstep";
+                builder.Password = "269f43dc";
+                builder.Database = "host1323541_itstep21";
+                builder.SslMode = MySqlSslMode.None;
+                connection = new MySqlConnection(builder.ToString());
+
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        public static MySqlCommand RunQuery(string query, string username)
+        {
+            try
+            {
+                if (connection != null)
+                {
+                    connection.Open();
+                    cmd = connection.CreateCommand();
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = query;
+                    cmd.Parameters.AddWithValue("@username", username);
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+            catch (Exception)
+            {
+                connection.Close();
+            }
+            return cmd;
+        }
+    }
+}
---
+++ db_chat/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace db_chat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DBHelper.EstablishConnection();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            Users aUser = UsersDA.RetrieveUser(username);
+            if (aUser.Password.Equals(password))
+            {
+                MessageBox.Show("Верно");
+                Process.Start("Client.exe");
+            }
+            else
+            {
+                MessageBox.Show("Не верно логин или пароль, еще раз пожалуйста");
+                txtUsername.Text = "";
+                txtPassword.Text = "";
+            }
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
