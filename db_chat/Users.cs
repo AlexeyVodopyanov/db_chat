a7cde38a37edb152680d3219640b5b2d4b4439cf
Алексей <52876090+AlexeyVodopyanov@users.noreply.github.com>
2021-04-10 14:27:21 +0300

    Add files via upload

---
+++ db_chat/Users.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace db_chat
+{
+    public class Users
+    {
+        private string username;
+        private string password;
+
+        public Users(string username, string password)
+        {
+            UserName = username;
+            Password = password;
+        }
+
+        public String UserName
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public String Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+    }
+}
---
+++ db_chat/UsersDA.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace db_chat
+{
+    public static class UsersDA
+    {
+        private static MySqlCommand cmd = null;
+        private static DataTable dt;
+        private static MySqlDataAdapter sda;
+
+        public static Users RetrieveUser(string username)
+        {
+            string query = "SELECT * FROM db where username = (@username) limit 1";
+            cmd = DBHelper.RunQuery(query, username);
+            Users aUser = null;
+            if (cmd != null)
+            {
+                dt = new DataTable();
+                sda = new MySqlDataAdapter(cmd);
+                sda.Fill(dt);
+                foreach (DataRow dr in dt.Rows)
+                {
+                    string uName = dr["username"].ToString();
+                    string password = dr["password"].ToString();
+                    aUser = new Users(uName, password);
+                }
+            }
+            return aUser;
+        }
+    }
+}
